28ca539cffd9364cb818a448328545779d8f7d37
golzek1 <golzek12@gmail.com>
2026-05-27 12:23:43 +0300

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	cacheComponents: true,
-	/* config options here */
 	experimental: {
 		authInterrupts: true,
+		missingSuspenseWithCSRBailout: false,
 	},
 }
-
-export default nextConfig
